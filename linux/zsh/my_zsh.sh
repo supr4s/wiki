01918e2c5cce47d4a0b3ad623d317b4a184a2e32
supr4s <34515269+supr4s@users.noreply.github.com>
2018-10-21 19:28:08 +0200

    Create my_zsh.sh

---
+++ linux/zsh/my_zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Install ZSH
+sudo apt-get update && sudo apt-get install zsh -y;
+
+#Récupération de oh-my-zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";
+
+#Définition de ZSH comme shell par défaut
+chsh;
+
+#Je change le thème par défaut par agnoster
+sed -i 's/robbyrussel/agnoster/g' ~/.zshrc && clear && source ~/.zshrc;
