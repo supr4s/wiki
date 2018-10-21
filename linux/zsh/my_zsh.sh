9593129184d82566ad166bd36b2989ed67b1a93d
supr4s <34515269+supr4s@users.noreply.github.com>
2018-10-21 19:48:37 +0200

    Update my_zsh.sh

--- linux/zsh/my_zsh.sh
+++ linux/zsh/my_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Install ZSH
-sudo apt-get update && sudo apt-get install zsh -y;
+sudo apt-get update && sudo apt-get install git zsh -y;
 
 #Récupération de oh-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)";
