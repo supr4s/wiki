eef9dbf7146f2a2c8065d86f2f2dbef20cacba13
supr4s <34515269+supr4s@users.noreply.github.com>
2018-10-21 20:15:44 +0200

    Update my_zsh.sh

--- linux/zsh/my_zsh.sh
+++ linux/zsh/my_zsh.sh
@@ -3,7 +3,10 @@
 sudo apt-get update && sudo apt-get install git zsh -y;
 
 #Récupération de oh-my-zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" && sed -i 's/robbyrussel/agnoster/g' ~/.zshrc && clear && source ~/.zshrc;
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#Je change manuellement le thème avec celui de agnoster (powerline shell)
+sed -i 's/robbyrussel/agnoster/g' ~/.zshrc && clear && source ~/.zshrc;
 
 #Définition de ZSH comme shell par défaut
 chsh;
