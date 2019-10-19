06d86a20c98461c36bbcd0d79d9a46ad720a79f2
supr4s <34515269+supr4s@users.noreply.github.com>
2019-10-19 13:17:27 +0200

    Create edge_router_x_unbound_error.sh

---
+++ linux/scripting/edge_router_x_unbound_error.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Put the script in /config/scripts/post-config.d with execution rights
+#Shutdown unbound service
+service unbound start
+#Delete root.key
+cd /var/lib/unbound && rm root.key
+#Retrieving the file
+wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /var/lib/unbound/root.hints
+#Start unbound service
+service unbound start
